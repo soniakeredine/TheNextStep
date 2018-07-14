6873daff93efe9e5c42185f3bdbefd24e71d5f5f
sonia keredine <sonia.keredine@hotmail.fr>
2018-07-14 19:17:09 +0200

    Add existing file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'dashboard/show'
   get 'static_pages/home'
+  get 'testdigital/testdigital'
+get 'static_pages/testdigital'
     get 'static_pages/index'
   get 'static_pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
